07e39e328353751fb36f033dc989f219f5abaf8c
Doreen9 <doreen.dodoo@turntabl.io>
2019-11-28 13:47:54 +0000

    Introduction to threading in java

---
+++ src/main/java/io/turntabl/my/mainThread.java
@@ -0,0 +1,45 @@
+package io.turntabl.my;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class mainThread {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            try {
+                Thread.sleep(10000);
+                System.out.println("Hello from " + Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            try {
+                Thread.sleep(10000);
+                System.out.println("Hello from " + Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        //setDaemon makes the thread background
+//        t1.setDaemon(true);
+//        t2.setDaemon(true);
+//        t1.start();
+//        t2.start();
+//        t1.join();
+//        t2.join();
+        System.out.println("Hello from main thread");
+
+
+       List<Thread> threads = new ArrayList<>();
+
+
+        IntStream.range(0, 10).forEach(i -> threads.add(new Thread()));
+//                .(i -> new Thread()).collect(Collectors.toList());
+
+        System.out.println(threads);
+    }
+}
